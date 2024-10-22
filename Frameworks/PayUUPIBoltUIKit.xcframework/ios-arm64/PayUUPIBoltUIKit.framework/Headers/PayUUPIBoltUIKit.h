56d93564ed5f3b9e198dbbc0b33492aec0c9b111
Amit Salaria <amit.salaria@payu.in>
2024-10-22 20:02:43 +0530

    Updated frameworks

---
+++ Frameworks/PayUUPIBoltUIKit.xcframework/ios-arm64/PayUUPIBoltUIKit.framework/Headers/PayUUPIBoltUIKit.h
@@ -0,0 +1,18 @@
+//
+//  PayUUPIBoltUI.h
+//  PayUUPIBoltUI
+//
+//  Created by Amit Salaria on 18/07/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PayUUPIBoltUI.
+FOUNDATION_EXPORT double PayUUPIBoltUIVersionNumber;
+
+//! Project version string for PayUUPIBoltUI.
+FOUNDATION_EXPORT const unsigned char PayUUPIBoltUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PayUUPIBoltUI/PublicHeader.h>
+
+
